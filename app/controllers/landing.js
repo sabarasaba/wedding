237f45a520fadf6e7ced9f247e7743463d4b4d72
Ignacio Rivas <rivasign@gmail.com>
2016-08-31 20:36:54 +0200

    Add dynamic form for guests

--- app/controllers/landing.js
+++ app/controllers/landing.js
@@ -1,7 +1,14 @@
 import Ember from 'ember';
+import AddGuest from '../components/add-guest';
 
 export default Ember.Controller.extend({
+  guests: [],
+
   actions: {
+    addGuest() {
+      this.get('guests').pushObject(1);
+    },
+
     sendRSVP() {
       const guest = this.store.createRecord('guest', {
         name: 'Ignacio Rivas',
