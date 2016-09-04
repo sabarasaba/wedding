e65f3e97bffafa79c4392e6de45f3292435a2268
Ignacio Rivas <rivasign@gmail.com>
2016-09-04 11:08:05 +0200

    Add meals

--- app/components/one-invitee.js
+++ app/components/one-invitee.js
@@ -1,10 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  starters: ['empanadita', 'chochito', 'chorizito'],
+  starters: [
+    'Smoked salmon roll with fluffy ricotta cheese filling and ginger apple sauce',
+    'Roasted tomato and eggplant salad with goat cheese, pine nuts, basil and pesto sauce',
+    'chorizito'
+  ],
   chosenStarter: null,
 
-  mains: ['meat', 'chicken', 'veggie'],
+  mains: [
+    'Zander fillet with fresh asparagus, cherry tomatoes and parsnip risotto in saffron sauce',
+    'Chicken fillet stuffed with sun-dried tomatoes, parmesan cheese and basil with small potatoes seasoned with thyme, celery and pumpkin puree, caramelized apricots',
+    'Beef tenderloin with cherry sauce, fried eggplant, balsamic onions, fried tomatoes, apricots, basil, potato flake'
+  ],
   chosenMain: null,
 
   actions: {
