10e11425febf24c9b4ed20c1e517bed44615af93
Ignacio Rivas <rivasign@gmail.com>
2016-09-06 08:32:19 +0200

    Add ganalytics

--- app/router.js
+++ app/router.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
+import googlePageview from './mixins/google-pageview';
 import config from './config/environment';
 
-const Router = Ember.Router.extend({
+const Router = Ember.Router.extend(googlePageview, {
   location: config.locationType,
   rootURL: config.rootURL
 });
