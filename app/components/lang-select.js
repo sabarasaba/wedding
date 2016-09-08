44d13c0873a7e28394cc8f212190d1f9c9edfc4d
Ignacio Rivas <rivasign@gmail.com>
2016-09-08 22:32:34 +0200

    Add lang selector

--- app/components/lang-select.js
+++ app/components/lang-select.js
@@ -7,7 +7,7 @@ export default Ember.Component.extend({
   locales: Ember.computed('i18n.locale', 'i18n.locales', function() {
     const i18n = this.get('i18n');
     return this.get('i18n.locales').map(function (loc) {
-      return { id: loc, text: i18n.t('language-select.language.' + loc) };
+      return { id: loc, text: loc };
     });
   }),
 
