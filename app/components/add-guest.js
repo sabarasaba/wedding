7c5c5a5e7788dad6cf32787514a658f03ed68ec5
Ignacio Rivas <rivasign@gmail.com>
2016-09-09 08:44:06 +0200

    Add guest component should use locale strings

--- app/components/add-guest.js
+++ app/components/add-guest.js
@@ -1,10 +1,26 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  starters: ['empanadita', 'chochito', 'chorizito'],
+  i18n: Ember.inject.service(),
+
   chosedStarter: null,
-  mains: ['meat', 'chicken', 'veggie'],
   chosedMain: null,
+
+  starters: Ember.computed('i18n.locale', function() {
+    return [
+      this.get('i18n').t('food.starter1'),
+      this.get('i18n').t('food.starter2'),
+      this.get('i18n').t('food.starter3')
+    ];
+  }),
+
+  mains: Ember.computed('i18n.locale', function() {
+    return [
+      this.get('i18n').t('food.main1'),
+      this.get('i18n').t('food.main2'),
+      this.get('i18n').t('food.main3')
+    ];
+  }),
 
   actions: {
     onStarterChanged(value) {
